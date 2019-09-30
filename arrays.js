d47bab6b98d4442312b2ce0191ab9bd5208bc909
impartial-cache-1887 <marciafreeman2011@gmail.com>
2019-09-30 19:23:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,10 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array,element) {
   var newArray = [element,...array]
-  return newArray;
+//  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
   array.push(element)
-  return array;
+//  return array;
 }
